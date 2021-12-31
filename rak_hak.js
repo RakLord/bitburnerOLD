0ba895da00dd8a95ed8ccf5af1464e096960252b
shaeca16 <48102560+shaeca16@users.noreply.github.com>
2021-12-31 03:25:35 +0000

    yes
    yes

---
+++ rak_hak.js
@@ -0,0 +1,48 @@
+//rak_hak.ns
+export async function main(ns) {
+	let targetServer = "foodnstuff";
+	let serversSeen = ns.scan("home");
+	let currentScan;
+	for (let i = 0; i < serversSeen.length; i++) {
+		currentScan = ns.scan(serversSeen[i])
+		for (var j = 0; j < currentScan.length; j++) {
+			if (serversSeen.indexOf(currentScan[j]) === -1) {
+				serversSeen.push(currentScan[j])
+			}
+		}
+	}    
+	
+	for (let i = 0; i < serversSeen.length; i++) {
+		var portsRequired = ns.getServerNumPortsRequired(serversSeen[i]);
+		var rootAccess = ns.hasRootAccess(serversSeen[i]);
+		var portsOpen = 0;
+
+		ns.tprint(rootAccess);
+		if (!rootAccess) {
+			if (ns.fileExists("BruteSSH.exe", "home")) {
+				ns.brutessh(serversSeen[i]);
+				portsOpen++;
+			}
+
+			if (portsOpen >= portsRequired) {
+				ns.nuke(serversSeen[i]);
+			}
+		}
+		ns.tprint(portsOpen)
+	}
+
+
+	var working = 0;
+	for (let i = 0; i < serversSeen.length; i++) {
+		var threadsToUse = Math.floor((ns.getServerMaxRam(serversSeen[i]) - ns.getServerUsedRam(serversSeen[i])) / ns.getScriptRam("vuln_server.ns", "home"));
+		await ns.scp("vuln_server.ns", "home", serversSeen[i]);
+		if (threadsToUse > 0) {
+			ns.tprint("[" + serversSeen[i] + "]" + "[" + threadsToUse + " threads] " + "Failed");
+			ns.exec("vuln_server.ns", serversSeen[i], threadsToUse, targetServer);
+			working++;
+		} else {
+			ns.tprint("[" + serversSeen[i] + "]" + "[" + threadsToUse + " threads] " + "Successful");
+		}
+	}
+	ns.tprint("Succesful: " + working);
+}
---
+++ vuln_server.js
@@ -0,0 +1,22 @@
+//vuln_server.ns
+let target_server;
+
+export async function main(ns) {
+	target_server = ns.args[0];
+	var moneyThresh = ns.getServerMaxMoney(target_server) * 0.75;
+	var securityThresh = ns.getServerMinSecurityLevel(target_server) + 5;
+	var rootAccess = ns.hasRootAccess;
+
+	if (rootAccess) {
+		while (true) {
+			if (ns.getServerSecurityLevel(target_server) > securityThresh) {
+				await ns.weaken(target_server);
+			} else if (ns.getServerMoneyAvailable(target_server) < moneyThresh) {
+				ns.print("Server money: " + ns.getServerMoneyAvailable + " | Threshold: " + moneyThresh);
+				await ns.grow(target_server);
+			} else {
+				await ns.hack(target_server);
+			}
+		}
+	}
+}
