dc100da996e847c35957ae9cdb1843b5f79ae40d
shaeca16 <48102560+shaeca16@users.noreply.github.com>
2021-12-31 19:49:48 +0000

    Reworked

--- rak_hak.js
+++ rak_hak.js
@@ -1,8 +1,16 @@
-//rak_hak.ns
+//rak_hak.js
 export async function main(ns) {
-	let targetServer = "foodnstuff";
-	let serversSeen = ns.scan("home");
-	let currentScan;
+	ns.disableLog("ALL");
+	var target;
+	if (ns.args[0]) {
+		target = ns.args[0];
+	} else 	target = "foodnstuff";
+
+	var hackScript = "vuln_server.js";
+
+	var serversSeen = ns.scan("home");
+	var currentScan;
+
 	for (let i = 0; i < serversSeen.length; i++) {
 		currentScan = ns.scan(serversSeen[i])
 		for (var j = 0; j < currentScan.length; j++) {
@@ -10,56 +18,23 @@ export async function main(ns) {
 				serversSeen.push(currentScan[j])
 			}
 		}
-	}    
-	
-	for (let i = 0; i < serversSeen.length; i++) {
-		var portsRequired = ns.getServerNumPortsRequired(serversSeen[i]);
-		var rootAccess = ns.hasRootAccess(serversSeen[i]);
-		var portsOpen = 0;
-
-		ns.tprint(rootAccess);
-		if (!rootAccess) {
-			if (ns.fileExists("BruteSSH.exe", "home")) {
-				ns.brutessh(serversSeen[i]);
-				portsOpen++;
-			}
-			if (ns.fileExists("FTPCrack.exe", "home")) {
-				ns.ftpcrack(serversSeen[i]);
-				portsOpen++;
-			}
-			if (ns.fileExists("relaySMTP.exe", "home")) {
-				ns.relaysmtp(serversSeen[i]);
-				portsOpen++;
-			}
-			if (ns.fileExists("HTTPWorm.exe", "home")) {
-				ns.httpworm(serversSeen[i]);
-				portsOpen++;
-			}
-			if (ns.fileExists("SQLInject.exe", "home")) {
-				ns.sqlinject(serversSeen[i]);
-				portsOpen++;
-			}
-
-			if (portsOpen >= portsRequired) {
-				ns.nuke(serversSeen[i]);
-				portsOpen++;
-			}
-		}
-		ns.tprint("PORTS OPENED: " + portsOpen);
 	}
-
+	ns.tprint(serversSeen);
 
 	var working = 0;
+	var failed = 0;
 	for (let i = 0; i < serversSeen.length; i++) {
-		var threadsToUse =  Math.floor((ns.getServerMaxRam(serversSeen[i]) - ns.getServerUsedRam(serversSeen[i])) / ns.getScriptRam("vuln_server.ns", "home"));
-		await ns.scp("vuln_server.ns", "home", serversSeen[i]);
+		var threadsToUse =  Math.floor((ns.getServerMaxRam(serversSeen[i]) - ns.getServerUsedRam(serversSeen[i])) / ns.getScriptRam(hackScript, "home"));
+		await ns.scp(hackScript, "home", serversSeen[i]);
 		if (threadsToUse > 0) {
-			ns.tprint("[" + serversSeen[i] + "]" + "[" + threadsToUse + " threads] " + "Failed");
-			ns.exec("vuln_server.ns", serversSeen[i], threadsToUse, targetServer);
+			ns.tprint("[" + serversSeen[i] + "]" + "[" + threadsToUse + " threads] " + "Successful");
+			ns.exec(hackScript, serversSeen[i], threadsToUse, target);
 			working++;
 		} else {
-			ns.tprint("[" + serversSeen[i] + "]" + "[" + threadsToUse + " threads] " + "Successful");
+			ns.tprint("[" + serversSeen[i] + "]" + "[" + threadsToUse + " threads] " + "Failed");
+			failed++;
 		}
 	}
 	ns.tprint("Succesful: " + working);
+	ns.tprint("Failed: " + failed);
 }
