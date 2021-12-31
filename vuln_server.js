7037223f69e0061e0908bdc1eef25a9b869d1590
shaeca16 <48102560+shaeca16@users.noreply.github.com>
2021-12-31 04:34:24 +0000

    yes

--- vuln_server.js
+++ vuln_server.js
@@ -1,7 +1,7 @@
 //vuln_server.ns
 let target_server;
-
 export async function main(ns) {
+	ns.disableLog("ALL");
 	target_server = ns.args[0];
 	var moneyThresh = ns.getServerMaxMoney(target_server) * 0.75;
 	var securityThresh = ns.getServerMinSecurityLevel(target_server) + 5;
@@ -10,11 +10,13 @@ export async function main(ns) {
 	if (rootAccess) {
 		while (true) {
 			if (ns.getServerSecurityLevel(target_server) > securityThresh) {
+				ns.print("Weakening server");
 				await ns.weaken(target_server);
 			} else if (ns.getServerMoneyAvailable(target_server) < moneyThresh) {
 				ns.print("Threshold %: " + (ns.getServerMoneyAvailable(target_server) / moneyThresh) * 100);
 				await ns.grow(target_server);
 			} else {
+				ns.print("Hacking server...")
 				await ns.hack(target_server);
 			}
 		}
