7ee0a6406d46c20d6de0f0b1c027fc9edcf75a6f
shaeca16 <48102560+shaeca16@users.noreply.github.com>
2022-01-01 01:37:43 +0000

    Update vuln_server.js

--- vuln_server.js
+++ vuln_server.js
@@ -1,24 +1,25 @@
 //vuln_server.js
 let target;
 export async function main(ns) {
-	ns.disableLog("ALL");
+	//ns.disableLog("ALL");
 	target = ns.args[0];
+	var securityOffset = 1;
 	var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
-	var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+	var securityThresh = ns.getServerMinSecurityLevel(target) + securityOffset;
 
 
 	while (true) {
 		if (ns.getServerSecurityLevel(target) > securityThresh) {
-			ns.print("[Wkn] Sec lvl: " + ns.getServerSecurityLevel(target) + " / 100");
+			ns.print("[W] Sec lvl: " + ns.getServerSecurityLevel(target) + " / 100");
 			await ns.weaken(target);
 
 		} else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
 			var thresholdPercent = (ns.getServerMoneyAvailable(target) / moneyThresh) * 100;
-			ns.print("[Grw] Cur Threshold: " + thresholdPercent + "%");
+			ns.print("[G] Cur Threshold: " + thresholdPercent + "%");
 			await ns.grow(target);
 
 		} else {
-			ns.print("[Hck] Money Available: $" + ns.getServerMoneyAvailable(target));
+			ns.print("[H] Money Available: $" + ns.getServerMoneyAvailable(target));
 			await ns.hack(target);
 		}
 	}
